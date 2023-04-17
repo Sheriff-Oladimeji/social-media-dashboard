49ba5c45b407a4d2f5225d79722c03433a75275f
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-04-17 16:29:01 +0100

    feat: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      Toggle: "hsl(210, 78%, 56%)",
+      ToggleTwo: "hsl(146, 68%, 55%)",
+      lightToggle: "hsl(230, 22%, 74%)",
+      VeryDarkBlueBG: "hsl(230, 17%, 14%)",
+      VeryDarkBlueTopBG: "hsl(232, 19%, 15%)",
+      DarkDesaturatedBlueCardBG: "hsl(228, 28%, 20%)",
+      DesaturatedBlueText: "hsl(228, 34%, 66%)",
+      White: "hsl(0, 0%, 100%)",
+      WhiteBG: "hsl(0, 0%, 100%)",
+      VeryPaleBlueTopBG: "hsl(225, 100%, 98%)",
+      LightGrayishBlueCardBG: "hsl(227, 47%, 96%)",
+      DarkGrayishBlueText: "hsl(228, 12%, 44%)",
+      VeryDarkBlueText: "hsl(230, 17%, 14%)",
+    },
   },
   plugins: [],
 };

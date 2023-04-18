7f4899eeb48440b66175857f585cac8e9fefbc6c
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-04-18 21:37:14 +0100

    add daa=sboard to APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from "./components/Navbar"
-
+import Dashboard from "./components/Dashboard"
 const App = () => {
   return (
     <div className="w-[90%] md:w-[70%] mx-auto">
-    
+    <Dashboard/>
     </div>
     )
     

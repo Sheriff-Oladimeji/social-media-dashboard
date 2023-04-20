b6f33e5efb4b1e99fad3318d1af92c969412eb95
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-04-20 19:21:46 +0100

    feat: add grid to overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -3,29 +3,19 @@ import {
     instagram, facebook, up, down,} from "../assets"
 const Overview = () => {
   return (
-      <aside>
-          <h1>Overview - Today</h1>
-          <div>
+      <aside className="font-Inter mt-6">
+          <h1 className="text-4xl text-DarkGrayishBlueText font-bold">Overview - Today</h1>
+          <div className="grid md:grid-cols-4 gap-8">
+             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
+             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
+             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
+             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
+             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
+             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
+             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
              <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
           </div>
-          <div>
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-          </div>
-          <div>
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-          </div>
-          <div>
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-          </div>
-          <div>
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-          </div>
-          <div>
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-          </div>
-          <div>
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-          </div>
+         
     </aside>
   )
 }

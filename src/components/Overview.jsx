50fe3fc09b56d1b6cef2a5a81109cf578e94a864
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-04-20 19:29:59 +0100

    fix: change card props in overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,19 +1,19 @@
 import CardInfo from "./CardInfo"
 import {
-    instagram, facebook, up, down,} from "../assets"
+    instagram, facebook, up, down, twitter, youtube,} from "../assets"
 const Overview = () => {
   return (
       <aside className="font-Inter mt-6">
-          <h1 className="text-4xl text-DarkGrayishBlueText font-bold">Overview - Today</h1>
+          <h1 className="text-4xl  md:text-3xl text-DarkGrayishBlueText font-bold mb-4">Overview - Today</h1>
           <div className="grid md:grid-cols-4 gap-8">
              <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
-             <CardInfo  action={'Page Views'} icon={facebook} count="87" arrow={up} value={3}/> 
+             <CardInfo  action={'Likes'} icon={facebook} count="52" arrow={down} value={2}/> 
+             <CardInfo  action={'Likes'} icon={instagram} count="5462" arrow={up} value={2257}/> 
+             <CardInfo  action={'Profile Views'} icon={instagram} count="52K" arrow={up} value={1375}/> 
+             <CardInfo  action={'Retweets'} icon={twitter} count="117" arrow={up} value={303}/> 
+             <CardInfo  action={'Likes'} icon={twitter} count="507" arrow={up} value={553}/> 
+             <CardInfo  action={'Likes'} icon={youtube} count="107" arrow={down} value={19}/> 
+             <CardInfo  action={'Page Views'} icon={youtube} count="140" arrow={down} value={12}/> 
           </div>
          
     </aside>

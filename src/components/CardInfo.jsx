3bb9e3457f498a58323a4c0fe7d856495b49bac4
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-04-20 14:10:03 +0100

    fix: remove bug for cardInfo display

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -2,12 +2,12 @@
 
 const CardInfo = ({action, icon, count, value, arrow}) => {
   return (
-      <div className="w-[100%] bg-LightGrayishBlueCardBG">
-          <div>
-              <p>{action}</p>
+      <div className="w-[100%] bg-LightGrayishBlueCardBG py-5 px-3 font-Inter">
+          <div className="flex justify-between items-center">
+              <p className="font-semibold text-DarkGrayishBlueText ">{action}</p>
               <img src={icon} alt="logo" />
           </div>
-          <div>
+          <div className="flex justify-between items-center">
               <h3>{count}</h3>
               <div>
                   <img src={arrow} alt="" />

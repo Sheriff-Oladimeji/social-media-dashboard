16a592d2f64da48ea83a2b26c044796941f019f3
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-04-21 14:59:36 +0100

    feat: add styles to Haeder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,17 +15,17 @@ const Header = () => {
       document.documentElement.classList.remove('dark');
     }
   }, [darkMode]);
-  const btnValue = darkMode?<FontAwesomeIcon icon="fa-solid fa-toggle-off" /> :<FontAwesomeIcon icon="fa-solid fa-toggle-on" /> 
+  const btnValue = darkMode?<FontAwesomeIcon icon="fa-solid fa-toggle-off"  className="text-5xl text-lightToggle"/> :<FontAwesomeIcon icon="fa-solid fa-toggle-on"  className="text-5xl text-lightToggle" /> 
 
   return (
     <header className="flex flex-col md:flex-row justify-between w-[90%] md:w-[70%] mx-auto  font-Inter">
-          <div>
-              <h1>Social Media Dashboard</h1>
-              <p>Total Followers: 23,004</p>
+          <div  className="mb-2 md:mb-0 border-b border-VeryDarkBlueText  pb-6">
+              <h1 className="text-3xl text-VeryDarkBlueText font-bold font-Inter">Social Media Dashboard</h1>
+        <p className="text-[16px] text-DarkGrayishBlueText font-semibold">Total Followers: 23,004</p>
           </div>
-          <div>
+          <div  className="flex justify-between md:justify-start items-center ">
               <p>Dark mode</p>
-        <button onClick={() => setDarkMode(!darkMode)}>{ btnValue}</button>
+        <button onClick={() => setDarkMode(!darkMode)} className="cursor-pointer outline-none border-none">{ btnValue}</button>
           </div>
     </header>
   )

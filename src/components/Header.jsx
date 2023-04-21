39bb2b01869d783708a8379876d9bbbd89920feb
Sheriff Oladimeji <dimejiademola5@gmail.com>
2023-04-21 17:41:04 +0100

    fix: change header positioning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,12 +18,12 @@ const Header = () => {
   const btnValue = darkMode?<FontAwesomeIcon icon="fa-solid fa-toggle-off"  className="text-5xl text-Toggle fill-ToggleTwo"/> :<FontAwesomeIcon icon="fa-solid fa-toggle-on"  className="text-5xl text-lightToggle" /> 
 
   return (
-    <header className="flex flex-col md:flex-row justify-between w-[90%] md:w-[80%] mx-auto  font-Inter">
-          <div  className="mb-2 md:mb-0 border-b border-VeryDarkBlueText  pb-6 md:border-none">
+    <header className="flex flex-col md:flex-row justify-between w-[90%] md:w-[80%] mx-auto items-center font-Inter py-4">
+          <div  className="mb-2 md:mb-0 border-b border-VeryDarkBlueText  pb-6 md:border-none items-center">
               <h1 className="text-3xl text-VeryDarkBlueText font-bold font-Inter dark:text-White">Social Media Dashboard</h1>
         <p className="text-[16px] text-DarkGrayishBlueText font-semibold">Total Followers: 23,004</p>
           </div>
-          <div  className="flex justify-between md:justify-start items-center ">
+          <div  className="flex justify-between md:justify-start items-center md:space-x-2">
         <p className="dark:text-DesaturatedBlueText font-semibold">Dark mode</p>
         <button onClick={() => setDarkMode(!darkMode)} className="cursor-pointer outline-none border-none">{ btnValue}</button>
           </div>

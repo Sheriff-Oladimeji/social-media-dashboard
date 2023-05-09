4137194a8972dbf0b5ce584975d733359a730912
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-09 16:47:49 +0100

    feat: add names to cards

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,11 +4,11 @@ import {
 const Dashboard = () => {
   return (
       <div className="grid md:grid-cols-4 gap-8 w-[90%] md:w-[80%] mx-auto  font-Inter mt-8">
-          <Card icon={facebook} style={"border-Facebook"} name={'@nathanf'} count={'1987'} todayCount={12} arrow={up} />
-      <Card icon={twitter}  style={"border-Twitter"}  name={'@nathanf'} count={1044} todayCount={99} arrow={up} />
-          <Card icon={instagram} style={"border-Instagram"}  name={'@realnathanf'} count={'11k'} todayCount={1099} arrow={up} />
+          <Card icon={facebook} style={"border-Facebook"} name={'Sheriff Oladimeji'} count={'1987'} todayCount={12} arrow={up} />
+      <Card icon={twitter}  style={"border-Twitter"}  name={'@SheriffWebDev'} count={1044} todayCount={99} arrow={up} />
+          <Card icon={instagram} style={"border-Instagram"}  name={'@SheriffOladimeji'} count={'11k'} todayCount={1099} arrow={up} />
       <Card icon={youtube} style={"border-YouTube"} 
-         name={'Nathan F.'} count={'8239'} todayCount={144} arrow={down} 
+         name={"Sheriff Oladimeji"} count={'8239'} todayCount={144} arrow={down} 
       />
     </div>
   )

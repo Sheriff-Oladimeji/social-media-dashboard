6a94dcec8e3ab170eed015ac4dd0f5894ad98358
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-24 21:18:09 +0100

    feat: add hover states for card background

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,21 +3,35 @@ import { up } from "../assets"
 
 const Card = ({icon, name, count, todayCount, style, arrow, link}) => {
   return (
-      <div className={`w-[100%] bg-VeryPaleBlueTopBG dark:bg-DarkDesaturatedBlueCardBG border-t-4 rounded-[4px] ${style}`}>
-          <div className="flex justify-center space-x-2 items-center mt-4">
-              <img src={icon} alt="icon"/>
-              <a className="dark:text-DesaturatedBlueText cursor-pointer" href={link} target="_blank">{name}</a>
-          </div>
-          <div className="flex justify-center flex-col items-center my-4">
-              <h1 className="font-bold text-5xl text-VeryDarkBlueText dark:text-White">{count}</h1>
-              <p className="font-normal text-DesaturatedBlueText mt-1">FOLLOWERS</p>
-          </div>
-          <div className={`flex justify-center items-center ${arrow === up?"text-LimeGreen":"text-BrightRed"} space-x-2 mb-4`}>
-              <img src={arrow} alt="icon" />
-              <p >{todayCount}Today</p>
-          </div>
+    <div
+      className={`w-[100%] bg-VeryPaleBlueTopBG dark:bg-VeryDarkBlueTopBG border-t-4 rounded-[4px] ${style} hover:bg-LightGrayishBlueCardBG cursor-pointer dark:hover:bg-DarkDesaturatedBlueCardBG `}
+    >
+      <div className="flex justify-center space-x-2 items-center mt-4">
+        <img src={icon} alt="icon" />
+        <a
+          className="dark:text-DesaturatedBlueText cursor-pointer"
+          href={link}
+          target="_blank"
+        >
+          {name}
+        </a>
+      </div>
+      <div className="flex justify-center flex-col items-center my-4">
+        <h1 className="font-bold text-5xl text-VeryDarkBlueText dark:text-White">
+          {count}
+        </h1>
+        <p className="font-normal text-DesaturatedBlueText mt-1">FOLLOWERS</p>
+      </div>
+      <div
+        className={`flex justify-center items-center ${
+          arrow === up ? "text-LimeGreen" : "text-BrightRed"
+        } space-x-2 mb-4`}
+      >
+        <img src={arrow} alt="icon" />
+        <p>{todayCount}Today</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Card
--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -3,21 +3,28 @@ import { up } from "../assets"
 
 const CardInfo = ({action, icon, count, value, arrow}) => {
   return (
-      <div className="w-[100%] bg-LightGrayishBlueCardBG py-5 px-3 font-Inter rounded-[6px] dark:bg-DarkDesaturatedBlueCardBG ">
-          <div className="flex justify-between items-center">
-              <p className="font-semibold text-DarkGrayishBlueText  dark:text-DesaturatedBlueText">{action}</p>
-              <img src={icon} alt="logo" />
-          </div>
-          <div className="flex justify-between items-center mt-6">
-              <h3 className="font-bold text-4xl text-VeryDarkBlueText dark:text-White">{count}</h3>
-              <div className={`flex items-center ${arrow === up?"text-LimeGreen": "text-BrightRed"} font-semibold`}>
-                  <img src={arrow} alt="" />
-                <p>{value}%</p>  
-              </div>
-              
-          </div>
+    <div className="w-[100%] bg-VeryPaleBlueTopBG py-5 px-3 font-Inter rounded-[6px] dark:bg-VeryDarkBlueTopBG dark:hover:bg-DarkDesaturatedBlueCardBG  hover:bg-LightGrayishBlueCardBG cursor-pointer">
+      <div className="flex justify-between items-center">
+        <p className="font-semibold text-DarkGrayishBlueText  dark:text-DesaturatedBlueText">
+          {action}
+        </p>
+        <img src={icon} alt="logo" />
+      </div>
+      <div className="flex justify-between items-center mt-6">
+        <h3 className="font-bold text-4xl text-VeryDarkBlueText dark:text-White">
+          {count}
+        </h3>
+        <div
+          className={`flex items-center ${
+            arrow === up ? "text-LimeGreen" : "text-BrightRed"
+          } font-semibold`}
+        >
+          <img src={arrow} alt="" />
+          <p>{value}%</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CardInfo

7a038d494a0c2bfe9e3b770fbf81096abe03ef38
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-05-24 21:04:59 +0100

    feat: add smooth-scroll effect to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,16 @@ import Overview from "./components/Overview"
 import Header from "./components/Header"
 const App = () => {
   return (
-    <section className="w-[100%] dark:bg-VeryDarkBlueBG">
-   
+    <section className="w-[100%] dark:bg-VeryDarkBlueBG scroll-smooth">
       <div className="w-[100%] bg-LightGrayishBlueCardBG dark:bg-VeryDarkBlueBG">
-         <Header/>
-    
+        <Header />
       </div>
-      
-        <Dashboard />
-    
-      <Overview/>
- 
-      </section>
-    )
+
+      <Dashboard />
+
+      <Overview />
+    </section>
+  );
     
 }
 
